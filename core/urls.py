267bf2683010f4154364074612432ef5f2fd1071
JeanP88 <locomrbeam88@gmail.com>
2024-11-06 21:55:01 -0500

    meando

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,8 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+    estoy meando
 """
 
 from django.contrib import admin
